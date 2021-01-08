26ce747b4530fcaa8b95b0a67f9bded7c8c560c2
Tim van Cann <timvancann@gmail.com>
2021-01-08 17:27:39 +0100

    Update to new Avri api

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="Unofficial wrapper around Avri endpoints",
     author="Tim van Cann",
     author_email="timvancann@gmail.com",
-    packages=['avri'],
+    packages=["avri"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/timvancann/pyavri",
@@ -18,9 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
-    install_requires=[
-        "requests>=2.22.0",
-        "pyfunctional>=1.3.0"
-    ]
+    python_requires=">=3.7",
+    install_requires=["requests>=2.22.0", "pyfunctional>=1.3.0"],
+    test_requires=['requests-mock==1.8.0']
 )
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,54 +1,101 @@
+import json
 from datetime import datetime
 
 import pytest
+import requests_mock
 
 from avri import api
 from avri.api import Cache, Garbage
+from avri.endpoints import BASE_URL, FETCH_ADDRESS
 
 
 @pytest.fixture
-def fake_response():
-    return """GFT;04-02-2020;21-01-2020;07-01-2020;
-PLASTIC;03-02-2020;13-01-2020;
-PAPIER;04-02-2020;07-01-2020;
-"""
+def fake_calendar():
+    return {
+        "dataList": [
+            {
+                "pickupDates": [
+                    "2021-01-21T00:00:00",
+                    "2021-02-04T00:00:00",
+                ],
+                "pickupType": 1,
+                "_pickupTypeText": "GREEN",
+            },
+            {
+                "pickupDates": [
+                    "2021-02-02T00:00:00",
+                    "2021-03-02T00:00:00",
+                    "2021-04-06T00:00:00",
+                ],
+                "pickupType": 2,
+                "_pickupTypeText": "PAPER",
+            },
+            {
+                "pickupDates": [
+                    "2021-02-09T00:00:00",
+                    "2021-03-02T00:00:00",
+                    "2021-03-23T00:00:00",
+                ],
+                "pickupType": 10,
+                "_pickupTypeText": "PACKAGES",
+            }
+        ],
+    }
+
+
+@pytest.fixture()
+def fake_address_id():
+    return {
+        "dataList": [
+            {
+                "UniqueId": "dave",
+            }
+        ],
+    }
 
 
 @pytest.fixture
-def client_with_cache(fake_response):
-    client = api.Avri('1234AB', '42')
-    client._cache = Cache(client.parse_content(fake_response), datetime.now())
+def client_with_cache(fake_calendar, fake_address_id):
+    client = api.Avri("1234AB", 42)
+    client._cache = Cache(client._parse_content(fake_calendar), datetime.now())
     return client
 
 
-def test_parse_content(fake_response):
-    client = api.Avri('1234AB', '42')
-    data = client.parse_content(fake_response).list()
-    assert len(data) == 7
-    assert len([_ for _ in data if _.name == 'gft']) == 3
-    assert len([_ for _ in data if _.name == 'plastic']) == 2
-    assert len([_ for _ in data if _.name == 'papier']) == 2
+def test_parse_content(fake_calendar):
+    client = api.Avri("1234AB", 42)
+    data = client._parse_content(fake_calendar)
+    assert len(data) == 8
+    assert len([_ for _ in data if _.name == "GREEN"]) == 2
+    assert len([_ for _ in data if _.name == "PAPER"]) == 3
+    assert len([_ for _ in data if _.name == "PACKAGES"]) == 3
+
+
+def test_parse_address(fake_address_id):
+    with requests_mock.Mocker() as m:
+        m.post(f'{BASE_URL}{FETCH_ADDRESS}', content=json.dumps(fake_address_id).encode())
+        client = api.Avri("1234AB", 42)
+        assert client.get_address_id() == "dave"
 
 
 def test_upcoming_new(client_with_cache):
-    assert client_with_cache.upcoming(datetime(2020, 1, 6)) == Garbage('gft', datetime(2020, 1, 7))
+    assert client_with_cache.upcoming(datetime(2021, 1, 21)) == Garbage("GREEN", datetime(2021, 1, 21))
 
 
 def test_upcoming_today(client_with_cache):
-    assert client_with_cache.upcoming(datetime(2020, 1, 7)) == Garbage('gft', datetime(2020, 1, 7))
+    assert client_with_cache.upcoming(datetime(2021, 2, 9)) == Garbage("PACKAGES", datetime(2021, 2, 9))
 
 
 def test_upcoming_of_each_new(client_with_cache):
     assert client_with_cache.upcoming_of_each(datetime(2020, 1, 6)) == [
-        Garbage('gft', datetime(2020, 1, 7)),
-        Garbage('papier', datetime(2020, 1, 7)),
-        Garbage('plastic', datetime(2020, 1, 13))
+        Garbage("GREEN", datetime(2021, 1, 21)),
+        Garbage("PAPER", datetime(2021, 2, 2)),
+        Garbage("PACKAGES", datetime(2021, 2, 9)),
     ]
 
 
 def test_upcoming_of_each_today(client_with_cache):
-    assert client_with_cache.upcoming_of_each(datetime(2020, 1, 7)) == [
-        Garbage('gft', datetime(2020, 1, 7)),
-        Garbage('papier', datetime(2020, 1, 7)),
-        Garbage('plastic', datetime(2020, 1, 13))
+    assert client_with_cache.upcoming_of_each(datetime(2021, 2, 3)) == [
+        Garbage("GREEN", datetime(2021, 2, 4)),
+        Garbage("PACKAGES", datetime(2021, 2, 9)),
+        Garbage("PAPER", datetime(2021, 3, 2)),
     ]
